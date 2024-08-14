3d035ea71e5928183a6d8c76e156dd30425f9900
microsizer201a <dimanalter@rambler.ru>
2024-08-14 16:26:46 +0300

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,16 @@
+# 1st programm
+print((9 ** 0.5) * 5)
+
+# 2nd programm
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd programm
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th programm
+s = "123.456"
+f = float(s) * 10
+print(int(f % 10))
+
